71808abb81aba69a3b631b171ac98e3b3de1c6a2
Nick Jewell <jewellnick3@gmail.com>
2023-09-12 09:54:47 -0500

    Initial Commit

---
+++ Hello World New/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello World New
+//
+//  Created by Nicholas T. Jewell on 9/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hello World New/Hello_World_NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_World_NewApp.swift
+//  Hello World New
+//
+//  Created by Nicholas T. Jewell on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Hello_World_NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
